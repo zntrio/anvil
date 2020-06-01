6d5f9c3f342fed3133784faeae1e801bc2aa7b50
Thibault NORMAND <me@zenithar.org>
2020-06-01 18:02:00 +0200

    chore(go): update license.

--- challenge.go
+++ challenge.go
@@ -1,3 +1,20 @@
+// Licensed to Anvil under one or more contributor
+// license agreements. See the NOTICE file distributed with
+// this work for additional information regarding copyright
+// ownership. Anvil licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may
+// not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
 package anvil
 
 import (
@@ -5,10 +22,11 @@ import (
 	"strings"
 	"time"
 
-	"go.zenithar.org/anvil/forge"
-	"go.zenithar.org/anvil/internal"
-	"go.zenithar.org/anvil/tap"
 	"golang.org/x/crypto/ed25519"
+
+	"zntr.io/anvil/forge"
+	"zntr.io/anvil/internal"
+	"zntr.io/anvil/tap"
 )
 
 // Meld a challenge from given credentials
